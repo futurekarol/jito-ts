058d1a838507815fffe6b9cda752bf154122a355
futurekarol <29566494+futurekarol@users.noreply.github.com>
2023-04-10 13:58:07 +0200

    try 4k

--- src/sdk/block-engine/searcher.ts
+++ src/sdk/block-engine/searcher.ts
@@ -325,7 +325,7 @@ export const searcherClient = (
     ChannelCredentials.createSsl(),
     {
       interceptors: [authInterceptor(authProvider)],
-      'grpc.keepalive_timeout_ms': 5000,
+      'grpc.keepalive_timeout_ms': 4000,
     }
   );
 
