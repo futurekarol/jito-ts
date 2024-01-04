9fe8a872c4b4317a2cbcde3d722b422df6e84bc4
futurekarol <29566494+futurekarol@users.noreply.github.com>
2024-01-04 20:23:39 +0100

    remove try catch in auth

--- src/sdk/block-engine/auth.ts
+++ src/sdk/block-engine/auth.ts
@@ -29,15 +29,11 @@ export const authInterceptor = (authProvider: AuthProvider): Interceptor => {
   return (opts: InterceptorOptions, nextCall: NextCall) => {
     return new InterceptingCall(nextCall(opts), {
       start: function (metadata: Metadata, listener: Listener, next) {
-        try {
-          const callback = (accessToken: Jwt) => {
-            metadata.set('authorization', `Bearer ${accessToken.token}`);
-            next(metadata, listener);
-          };
-          authProvider.injectAccessToken(callback);
-        } catch (e) {
-          console.log(e);
-        }
+        const callback = (accessToken: Jwt) => {
+          metadata.set('authorization', `Bearer ${accessToken.token}`);
+          next(metadata, listener);
+        };
+        authProvider.injectAccessToken(callback);
       },
     });
   };
